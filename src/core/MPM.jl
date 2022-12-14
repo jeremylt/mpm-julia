da18009fa7d60449dbe5410a0eaeeb8bc36158fb
Jeremy L Thompson <jeremy@jeremylt.org>
2022-12-14 16:33:52 -0700

    minor rearrange to better see parts

--- src/core/MPM.jl
+++ src/core/MPM.jl
@@ -2,11 +2,17 @@
 # Collects all files needed for main drivers
 # ------------------------------------------------------------------------------
 
+# Material points
 include("MaterialPoint.jl")
 include("BoxMaterialDomain.jl")
 include("DiscMaterialDomain.jl")
+
+# Background grid
 include("GridPoint.jl")
 include("Grid.jl")
+
+# Material point and grid transfer
 include("Basis.jl")
+include("Transfer.jl")
 
 # ------------------------------------------------------------------------------
